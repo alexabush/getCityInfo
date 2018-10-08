2d85209ad2a0ca4823303030e0d2b98d9c09f119
Alex Bush <alexabush7@gmail.com>
2018-10-08 11:02:52 -0700

    made improvements to city lookup by number

--- index.js
+++ index.js
@@ -1,37 +1,45 @@
-console.log('hello world from sample.js');
-// searchBar = document.querySelector('.gLFyf');
-// searchBar.value = 'Hello Sir, what may I search for you today?';
 $(async function() {
-  let $myInput = $('#myInput');
-  let $form = $('#myForm');
-  let $inputDisplay = $('#inputDisplay');
-  let $cityList = $('#cityList');
+  console.log('hello world from sample.js');
+  const $myInput = $('#myInput');
+  const $form = $('#myForm');
+  const $inputDisplay = $('#inputDisplay');
+  const $cityList = $('#cityList');
   const endpoint =
     'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+  const data = await fetchData(endpoint);
+  let $submissionStatus = $('#submissionStatus');
 
   $myInput.on('keyup', async e => {
-    let input = $(e.currentTarget).val();
-    $inputDisplay.text(input);
+    const inputStr = $(e.currentTarget).val();
+    const inputNum = +inputStr - 1;
+    let city;
+    if (data[inputNum]) {
+      city = data[inputNum].city;
+    } else {
+      city = 'No city found';
+    }
+    $inputDisplay.text(city);
   });
 
   $form.on('submit', async e => {
     e.preventDefault();
-    let input = $(e.target).find('input').val();
-    let data = await fetchData(endpoint);
-    let city;
-    if (data[input]) {
-      city = data[input].city;
+    const inputStr = $(e.target)
+      .find('input')
+      .val();
+    const inputNum = +inputStr - 1;
+    if (data[inputNum]) {
+      let city = data[inputNum].city;
+      const cityLi = $('<li>').text(city);
+      $cityList.prepend(cityLi);
     } else {
-      city = 'No city found';
+      $submissionStatus.text('Not a valid number of city!');
     }
-    let cityLi = $('<li>').text(city);
-    $cityList.prepend(cityLi);
   });
 
   async function fetchData(url) {
     try {
-      let res = await fetch(url);
-      let rawData = await res.json();
+      const res = await fetch(url);
+      const rawData = await res.json();
       return rawData;
     } catch (error) {
       return console.log(error);
